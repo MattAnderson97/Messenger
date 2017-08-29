4b2aaece2155f41c6cc2215d542ee64b5a3b30be
wolv <pragfang@gmail.com>
2017-08-29 14:47:28 +0100

    push changes

--- src/main/java/space/wolv/chatplus/ChatPlus.java
+++ src/main/java/space/wolv/chatplus/ChatPlus.java
@@ -2,20 +2,27 @@ package space.wolv.chatplus;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import space.wolv.chatplus.events.PlayerChatListener;
 
-public class ChatPlus extends JavaPlugin
-{
+import java.io.File;
+
+public class ChatPlus extends JavaPlugin {
     private static FileConfiguration config;
 
     @Override
-    public void onEnable()
-    {
+    public void onEnable() {
+        File file = new File(getDataFolder() + File.separator + "config.yml");
+        if (!file.exists()) {
+            saveDefaultConfig();
+        }
+
         config = getConfig();
+
+        getServer().getPluginManager().registerEvents(new PlayerChatListener(), this);
     }
 
     @Override
-    public void onDisable()
-    {
+    public void onDisable() {
 
     }
 
--- src/main/java/space/wolv/chatplus/events/PlayerChatListener.java
+++ src/main/java/space/wolv/chatplus/events/PlayerChatListener.java
@@ -1,6 +1,11 @@
 package space.wolv.chatplus.events;
 
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -13,30 +18,88 @@ public class PlayerChatListener implements Listener
     @EventHandler(priority = EventPriority.HIGHEST)
     public void on(AsyncPlayerChatEvent event)
     {
-        if (event.isCancelled() || !ChatPlus.getConfigData().getBoolean("customChatEnabled"))
+        FileConfiguration config = ChatPlus.getConfigData();
+
+        if (event.isCancelled() || !config.getBoolean("chat.enableCustomChat"))
+        {
             return;
+        }
 
         event.setCancelled(true);
 
         Player player = event.getPlayer();
         String message = event.getMessage();
-        String newMessage = parse(ChatPlus.getConfigData().getString("chatStyle"), player, message);
 
-        Bukkit.getOnlinePlayers().forEach(loopPlayer -> loopPlayer.sendMessage(newMessage));
+        TextComponent newMessage = parse(config.getString("chat.chatStyle"), player, message);
+        Bukkit.getOnlinePlayers().forEach(loopPlayer -> loopPlayer.spigot().sendMessage(newMessage));
     }
 
-    public String parse(String toParse, Player player, String originalMessage)
+    private TextComponent parse(String toParse, Player player, String originalMessage)
     {
-        String[] splitMessage = toParse.split("||");
+        TextComponent json = new TextComponent();
 
-        for (int i = 0; i < splitMessage.length; i++)
+        String parsedMessage = toParse.replaceAll("%player%", player.getName())
+                                     .replaceAll("%displayname%", player.getDisplayName())
+                                     .replaceAll("%message%", originalMessage);
+
+        return parseJson(parsedMessage);
+    }
+
+    private TextComponent parseJson(String message)
+    {
+        TextComponent json = null;
+
+        if (message.contains("||"))
         {
-            String messagePart = splitMessage[i];
-            splitMessage[i] = messagePart.replaceAll("%player%", player.getName());
-            splitMessage[i] = messagePart.replaceAll("%displayname%", player.getDisplayName());
-            splitMessage[i] = messagePart.replaceAll("%message%", originalMessage);
+            Bukkit.getServer().broadcastMessage("json!");
+
+            String[] splitMessage = message.split("\\|\\|");
+            TextComponent curLine = null;
+
+            for (String line : splitMessage)
+            {
+
+                if (line.startsWith("ttp:"))
+                {
+                    Bukkit.getServer().broadcastMessage("tooltip");
+                    ComponentBuilder toolTip = new ComponentBuilder(line.replace("ttp:", "").trim());
+                    curLine.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, toolTip.create()));
+                }
+                else if (line.startsWith("cmd:"))
+                {
+                    curLine.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, line.replace("cmd:", "")));
+                }
+                else if (line.startsWith("sgt:"))
+                {
+                    curLine.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, line.replace("sgt:", "")));
+                }
+                else if (line.startsWith("url:"))
+                {
+                    curLine.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, line.replace("url:", "").trim()));
+                }
+                else
+                {
+                    if (curLine != null)
+                    {
+                        if (json != null)
+                        {
+                            json.addExtra(" " + curLine);
+                        }
+                        else
+                        {
+                            json = curLine;
+                        }
+                    }
+
+                    curLine = new TextComponent(line);
+                }
+            }
+        }
+        else
+        {
+            json.setText(message);
         }
 
-        return String.join(" ", splitMessage);
+        return json;
     }
 }
